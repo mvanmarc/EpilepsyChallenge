967a7b0ebd7686621617c91efaa1b23b64987dcd
Maarten Vanmarcke <mvanmarc@krakas.esat.kuleuven.be>
2025-01-20 08:43:20 +0100

    Change data path from TUH_eeg to TUH_eeg_seizures

--- TUHProcesser/createListOfRecordings.py
+++ TUHProcesser/createListOfRecordings.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 # Script to create the list of patients/recordings/edfs #
 #########################################################
 
-# Example of path: /esat/biomeddata/kkontras/TUH/tuh_eeg/tuh_eeg/v2.0.1/edf/000/aaaaaaaa/s001_2015/01_tcp_ar/aaaaaaaa_s001_t000.edf
+# Example of path: /esat/biomeddata/kkontras/TUH/tuh_eeg/tuh_eeg_seizure/v2.0.3/edf/train/aaaaaaac/s001_2002/02_tcp_le/aaaaaaac_s001_t000.edf
 
 def getSubFolders(path: Path) -> list:
     """ Get list of all subfolders in a given folder """
@@ -26,39 +26,41 @@ def getSubEDFFiles(path: Path) -> list:
     except Exception as exc:
         print(exc)
         return []
+    
+def isAnnotationAvailable(path: Path) -> bool:
+    """ Check whether a given EDF file has an annotation file available. """
+    ann_path = path[:-4] + ".csv_bi"
+    return os.path.isfile(ann_path)
 
 # Specify paths
 out_path = Path('dataset_lists') # Path for output files
-data_path = Path('/esat/biomeddata/kkontras/TUH/tuh_eeg/tuh_eeg/v2.0.1/edf/') # Path of data files
+data_path = Path('/esat/biomeddata/kkontras/TUH/tuh_eeg/tuh_eeg_seizure/v2.0.3/edf/train') # Path of data files
 
-# Construct and save list of patient groups, subject ids and recordings
-sub_grp = getSubFolders(data_path)
-with open(os.path.join(out_path, 'groups.tsv'), 'w', newline='') as tsvfile_grps:
-    writer_grps = csv.writer(tsvfile_grps, delimiter='\t', lineterminator='\n')
-    writer_grps.writerow(["group_id", "List of patient ids"])
-    with open(os.path.join(out_path, 'recordings.tsv'), 'w', newline='') as tsvfile_recs:
-        writer_recs = csv.writer(tsvfile_recs, delimiter='\t', lineterminator='\n')
-        writer_recs.writerow(["group_id", "subject_id", "session","montage", "recording", "path"])
-        with open(os.path.join(out_path, 'subject_ids.tsv'), 'w', newline='') as tsvfile_subs:
-            writer_subs = csv.writer(tsvfile_subs, delimiter='\t', lineterminator='\n')
-            writer_subs.writerow(["subject_id"])
+# Construct and save list subject ids and recordings
+with open(os.path.join(out_path, 'recordings.tsv'), 'w', newline='') as tsvfile_recs:
+    writer_recs = csv.writer(tsvfile_recs, delimiter='\t', lineterminator='\n')
+    writer_recs.writerow(["group_id", "subject_id", "session","montage", "recording", "path"])
+    with open(os.path.join(out_path, 'subject_ids.tsv'), 'w', newline='') as tsvfile_subs:
+        writer_subs = csv.writer(tsvfile_subs, delimiter='\t', lineterminator='\n')
+        writer_subs.writerow(["subject_id"])
 
+        # Get list of all patients
+        sub_ids = getSubFolders(data_path)
+        writer_subs.writerows([[x] for x in sub_ids])
 
-            for grp in tqdm(sub_grp):
-                sub_ids = getSubFolders(os.path.join(data_path,grp))
-
-                writer_grps.writerow([grp] + sub_ids)
-                writer_subs.writerows([[x] for x in sub_ids])
-
-                for sub in sub_ids:
-                    sessions = getSubFolders(os.path.join(data_path,grp,sub))
-
-                    for sess in sessions:
-                        monts = getSubFolders(os.path.join(data_path,grp,sub,sess))
-                        
-                        for mont in monts:
-                            recs = getSubEDFFiles(os.path.join(data_path,grp,sub,sess, mont))
-                            writer_recs.writerows([[grp, sub, sess, mont, rec, os.path.join(data_path,grp,sub,sess, mont, rec)] for rec in recs])
+        # Get list of all recordings
+        print("Extract each recording for each out of", len(sub_ids),"patients:")
+        for sub in tqdm(sub_ids):
+            sessions = getSubFolders(os.path.join(data_path,sub))
+            for sess in sessions:
+                monts = getSubFolders(os.path.join(data_path,sub,sess))
+                for mont in monts:
+                    recs = getSubEDFFiles(os.path.join(data_path,sub,sess, mont))
+                    for rec in recs:
+                        rec_path = os.path.join(data_path,sub,sess, mont, rec)
+                        writer_recs.writerow([sub, sess, mont, rec, rec_path])
+                        if not isAnnotationAvailable(rec_path):
+                            print("Annotation not found of recording:", rec_path)
 
 
 # with h5py.File(os.path.join(config.save_dir, 'predictions', name, rec[0] + '_' + rec[1] + '_preds.h5'), 'w') as f:
